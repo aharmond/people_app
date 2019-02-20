f9ea02ae601e7576187dede268c374deb0800592
AJ Harmond <ajharmond@gmail.com>
2019-02-19 20:09:05 -0700

    Add dog model/index/CRUD functionality and home page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_215144) do
+ActiveRecord::Schema.define(version: 2019_02_20_022925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "breed"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "first_name"
