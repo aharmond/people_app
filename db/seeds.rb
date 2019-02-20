4c98e12d5c8910233b09f70982d11ce9072a5b40
AJ Harmond <ajharmond@gmail.com>
2019-02-19 19:16:37 -0700

    Add to db/seed (edit to faker so might not work easily)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do
+  person = Person.new
+  person.gender = Faker::Gender.type
+  if person.gender == "Female"
+    person.first_name = Faker::Name.female_first_name
+  elsif person.gender == "Male"
+    person.first_name = Faker::Name.male_first_name
+  else
+    person.first_name = Faker::Name.first_name
+  end
+  person.last_name = Faker::Name.last_name
+  person.age = Faker::Number.between(18, 75)
+  person.hair_color = Faker::Color.hair_color
+  person.eye_color = Faker::Color.eye_color
+  person.save!
+end
